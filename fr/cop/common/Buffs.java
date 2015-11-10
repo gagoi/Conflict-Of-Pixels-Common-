8161640db43563394c263abf97019b67589f8376
Gagoi <harrygag@gmail.com>
2015-11-10 23:10:57 +0100

    Les Buffs ont désormais un type (erreur de commit précedent ?)

--- fr/cop/common/Buffs.java
+++ fr/cop/common/Buffs.java
@@ -5,6 +5,12 @@ public class Buffs {
 
 	//Classe similaire à Stats.class
 	
+	public static final int TYPE_TARGETED = 0;
+	public static final int TYPE_ZONE = 0;
+	public static final int TYPE_ENEMY = 0;
+	public static final int TYPE_ALLY = 0;
+	
+	
 	public Buffs() {
 	}
 
