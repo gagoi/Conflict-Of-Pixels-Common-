668a5cb5e87ca228e49db07cf5436e1c5993d5eb
Gagoi <harrygag@gmail.com>
2015-11-17 23:07:29 +0100

    Ajouts au profil permettant de l'intégrer à la barre de profil du launcher.

--- fr/cop/common/Profil.java
+++ fr/cop/common/Profil.java
@@ -3,9 +3,11 @@ package fr.cop.common;
 public class Profil {
 
 	int wins = 52;
+	String nickname, UUID;
+	int xpAmmount = 150;
 
 	public Profil() {
-
+		nickname = "Gagoi";
 	}
 
 	public int getWin() {
@@ -14,5 +16,29 @@ public class Profil {
 
 	public String getFormattedKDA() {
 		return ("Kills : " + 2 + "\rDeaths : " + 3);
+	}
+	
+	public int getXpAmmount(){
+		return xpAmmount;
+	}
+	
+	public String getNickname(){
+		return nickname;
+	}
+	
+	public String getUUID(){
+		return "player:AAAABBBBCCCC0000";
+	}
+
+	public float getXpAmmountInLevel() {
+		return getXpAmmount()/getNextLevelNeededXp();
+	}
+
+	public float getNextLevelNeededXp() {
+		return getXpAmmount()*2;
+	}
+
+	public int getIcon() {
+		return 0;
 	}
 }
