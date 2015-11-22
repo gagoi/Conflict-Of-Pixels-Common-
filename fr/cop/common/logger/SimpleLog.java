4bd9df3a34f1ec98849204f39d8394c71dfde41a
Gagoi <harrygag@gmail.com>
2015-11-22 16:49:28 +0100

    Le logger conserve également l'heure.

--- fr/cop/common/logger/SimpleLog.java
+++ fr/cop/common/logger/SimpleLog.java
@@ -5,12 +5,14 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Calendar;
 
 import fr.cop.common.Game;
 
 public class SimpleLog {
 
 	public File logFile = new File(Game.gameFolder, "log.txt");
+	private Calendar cal = Calendar.getInstance();
 	private PrintWriter writer;
 
 	public SimpleLog() {
@@ -19,7 +21,7 @@ public class SimpleLog {
 			logFile.createNewFile();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}	
+		}
 
 		try {
 			writer = new PrintWriter(new FileOutputStream(logFile));
@@ -29,14 +31,20 @@ public class SimpleLog {
 	}
 
 	public void logTxt(String tag, String message) {
-		System.out.println(tag + " : " + message);
-		writer.println(tag + " : " + message);
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		int min = cal.get(Calendar.MINUTE);
+		int sec = cal.get(Calendar.SECOND);
+		System.out.println("[" + hour + "H " + min + "M " + sec + "S] :: " + tag + " : " + message);
+		writer.println("[" + hour + "H " + min + "M " + sec + "S] :: " + tag + " : " + message);
 		writer.flush();
 	}
 
 	public void logErr(String tag, String message) {
-		System.err.println("/!\\ERROR/!\\" + tag + " : " + message);
-		writer.println("/!\\ERROR/!\\" + tag + " : " + message);
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		int min = cal.get(Calendar.MINUTE);
+		int sec = cal.get(Calendar.SECOND);
+		System.err.println("[" + hour + "H " + min + "M " + sec + "S] :: " + "/!\\ERROR/!\\" + tag + " : " + message);
+		writer.println("[" + hour + "H " + min + "M " + sec + "S] :: " + "/!\\ERROR/!\\" + tag + " : " + message);
 		writer.flush();
 	}
 }
